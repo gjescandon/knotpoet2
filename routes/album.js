ea8b9cec60272993ce151a6fee9a0eed925faa70
gjescandon <gjescandon@gmail.com>
2020-10-25 12:48:43 -0400

    fix ico

--- routes/album.js
+++ routes/album.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('album', { title: 'album knot' });
+  res.render('album', { title: 'knot hear' });
 });
 
 module.exports = router;
